3e5df8526a7281b3f7288434909abc15eb7c2b64
GithubJjang <96365979+GithubJjang@users.noreply.github.com>
2025-05-09 16:30:54 +0900

    Update firebase.js
    change

--- src/apis/firebase.js
+++ src/apis/firebase.js
@@ -8,13 +8,13 @@ import { getStorage } from "firebase/storage"
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBTLAv6wGA--ago8nUor445hdho3eIvqnA",
-  authDomain: "authfirewithspringboot.firebaseapp.com",
-  projectId: "authfirewithspringboot",
-  storageBucket: "authfirewithspringboot.appspot.com",
-  messagingSenderId: "477737423484",
-  appId: "1:477737423484:web:d5afec2df2a5c2e3bbaff9",
-  measurementId: "G-5FXBC2P096"
+  apiKey: "***",
+  authDomain: "***",
+  projectId: "***",
+  storageBucket: "***",
+  messagingSenderId: "***",
+  appId: "***",
+  measurementId: "***"
 };
 
 // Initialize Firebase
